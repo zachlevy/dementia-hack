8c8d44371d7ff1ae8775d66dca33468d436181a3
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-13 15:24:22 -0400

    new slides

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -5,6 +5,7 @@ class SlidesController < ApplicationController
 
   def new
     @slide = Slide.new
+    puts "new slide page"
   end
 
   def show
@@ -12,8 +13,33 @@ class SlidesController < ApplicationController
   end
 
   def edit
+    @slide = Slide.find(params[:id])
   end
 
   def destroy
   end
+
+  def create
+    redirect_to root_path
+    @slide = Slide.new(slide_params)
+
+    if @slide.save
+      puts "slide saved"
+    end
+  end
+
+  def update
+    @slide.update(slide_params)
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def slide_params
+      params.require(:slide).permit(
+          :slide_html,
+          :slide_weight,
+          :slide_length,
+          :slideshow_id,
+        )
+    end
 end
