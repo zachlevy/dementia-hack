d6288ad19f3710cf6c5585bf6bf3d4f2e79bbea4
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-13 13:15:42 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Slideshow.create()
+
+Slide.create!(
+	slide_html: "<div class=\"test-class\"><img src=\"http://placehold.it/1920x1080&text=slide1\"/></div>",
+	slide_weight: 20,
+	slide_length: 3000,
+	slideshow_id: 1
+)
+Slide.create!(
+	slide_html: "<div class=\"test-class\"><img src=\"http://placehold.it/1920x1080&text=slide2\"/></div>",
+	slide_weight: 40,
+	slide_length: 3000,
+	slideshow_id: 1,
+)
+Slide.create!(
+	slide_html: "<div class=\"test-class\"><img src=\"http://placehold.it/1920x1080&text=slide3\"/></div>",
+	slide_weight: 60,
+	slide_length: 5000,
+	slideshow_id: 1,
+)
