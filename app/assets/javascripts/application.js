e867a4a13e7a6544a46194b902f750c6b6dd9348
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-13 23:03:41 -0400

    video template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,51 @@
 
 $(function() {
 	console.log("app.js");
+	// when select list changed
+	$('#slide-type-select').change(function() {
+		slideType = $("#slide-type-select option:selected").val();
+
+		// reset template
+		$(".template").addClass("hidden");
+		$("#slide_slide_html").value = "";
+
+		// reveal template
+		$("#" + slideType + "-template").removeClass("hidden");
+
+		$("#video-template input").keyup(function () {
+			updateVideoTemplate();
+			console.log($(this).val());
+		});
+	});
 });
+
+function onVideoClick (videoId) {
+    var playerHtml = '<iframe class="player" src="//www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0&loop=0&theme=light&color=white&iv_load_policy=3" frameborder="0" allowfullscreen=""></iframe>';
+    document.getElementById('yt-' + videoId).innerHTML = playerHtml;
+
+    var html = "";
+	html =+ '<iframe class="player" src="//www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0&loop=0&theme=light&color=white&iv_load_policy=3" frameborder="0" allowfullscreen=""></iframe>';
+
+
+}
+
+function playVideo (videoId) {
+	html = '<iframe class="player" src="//www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0&loop=0&theme=light&color=white&iv_load_policy=3" frameborder="0" allowfullscreen=""></iframe>';
+	$("#" + videoId).html(html);
+}
+
+function updateVideoTemplate () {
+	var youtubeURL = $("#video-url").val();
+
+	var videoId = youtubeURL.split('v=')[1];
+	var ampersandPosition = videoId.indexOf('&');
+	if(ampersandPosition != -1) {
+		videoId = videoId.substring(0, ampersandPosition);
+	}
+	console.log(videoId);
+
+	html = '<div class="slide-type-video"><div id="' + videoId + '"></div><iframe class="player" src="//www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0&loop=0&theme=light&color=white&iv_load_policy=3" frameborder="0" allowfullscreen=""></iframe></div>';
+	$("#slide_slide_html").val(html);
+}
+
+
